47dca823433192885c7e493d1e28f84e3d6240a3
Joseph Leon <joseph_jiy@hotmail.com>
2015-02-17 18:26:54 -0500

    contact form layout

--- app/src/main/java/com/lunarwolf/market/view/ContactFormActivity.java
+++ app/src/main/java/com/lunarwolf/market/view/ContactFormActivity.java
@@ -1,6 +1,5 @@
 package com.lunarwolf.market.view;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,26 +7,27 @@ import android.view.MenuItem;
 
 import com.lunarwolf.market.R;
 
-public class ShoppingCartActivity extends ActionBarActivity {
+public class ContactFormActivity extends ActionBarActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_shopping_cart);
+    setContentView(R.layout.activity_contact_form);
   }
 
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_shopping_cart, menu);
+    getMenuInflater().inflate(R.menu.menu_contact_form, menu);
     return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == R.id.action_settings) {
-      startActivity(new Intent(this, ContactFormActivity.class));
+    int id = item.getItemId();
+
+    if (id == R.id.action_settings) {
       return true;
     }
 
